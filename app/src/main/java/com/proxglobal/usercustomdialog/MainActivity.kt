606e4b669ab8c4ed4823bfecafdd6b654bb16d81
phongnn <phongnguyennhu1522@gmail.com>
2023-08-31 11:31:25 +0700

    Initialize Commit 1

--- app/src/main/java/com/proxglobal/usercustomdialog/MainActivity.kt
+++ app/src/main/java/com/proxglobal/usercustomdialog/MainActivity.kt
@@ -2,10 +2,38 @@ package com.proxglobal.usercustomdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.proxglobal.usercustomdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
+
+        activityBinding.btnOpenOutOfUseDialog.setOnClickListener {
+            val runOutOfUsesDialog = RunOutOfUsesDialog(this, object :
+                RunOutOfUsesDialog.RunOutOfUseDialogListener {
+                override fun onConfirm(text: String) {
+                    Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+                }
+            })
+            runOutOfUsesDialog.show()
+        }
+
+        activityBinding.btnOpenFreeUseDialog.setOnClickListener {
+            val freeUsesDialog =
+                FreeUsesDialog(this, object : FreeUsesDialog.FreeUsesDialogListener {
+                    override fun onConfirm(text: String) {
+                        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+            freeUsesDialog.show()
+        }
     }
+
 }
